6eab21aa0f6a609c751bc29fb9174938506dae9a
Andreas Richter <ar@anrichter.net>
2019-02-08 21:36:53 +0100

    Refactor: Extract new classes
    * move GitLab stuff to GitLabConnector
    * move GitHub stuff to GitHubConnector

--- GitLabToGitHub/GitHubConnector.cs
+++ GitLabToGitHub/GitHubConnector.cs
@@ -1,4 +1,7 @@
-﻿using Octokit;
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Octokit;
 
 namespace GitLabToGitHub
 {
@@ -12,6 +15,29 @@ namespace GitLabToGitHub
             {
                 Credentials = new Credentials(gitHubSettings.AccessToken)
             };
+        }
+
+        public NewRepository GetNewRepository(string sourceGroupName, string sourceProjectName)
+        {
+            sourceGroupName = Regex.Replace(sourceGroupName, @"\s+", string.Empty);
+            sourceProjectName = Regex.Replace(sourceProjectName, @"\s+", string.Empty);
+            var repositoryName = $"{sourceGroupName}_{sourceProjectName}";
+
+            Console.WriteLine($"New GitHub Repository name [{repositoryName}]: ");
+            var userInput = Console.ReadLine();
+            repositoryName = string.IsNullOrEmpty(userInput) ? repositoryName : userInput;
+            var newRepository = new NewRepository(repositoryName);
+
+            Console.WriteLine($"Should Repository {repositoryName} be Private or Public [R/u]?");
+            var allowedKeys = new[] { ConsoleKey.R, ConsoleKey.U, ConsoleKey.Enter };
+            ConsoleKeyInfo userInputKeyInfo;
+            do
+            {
+                userInputKeyInfo = Console.ReadKey(false);
+            } while (!allowedKeys.Contains(userInputKeyInfo.Key));
+            newRepository.Private = userInputKeyInfo.Key != ConsoleKey.U;
+
+            return newRepository;
         }
     }
 }
